d7f2e4090f13ab5d8f41323994b3b187dbc80477
Gad-Ongoro <gadongoro1@gmail.com>
2024-04-14 17:33:22 +0300

    feat: Simple Web Scrapper

--- lib/scraper.py
+++ lib/scraper.py
@@ -4,3 +4,12 @@ import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
 html = requests.get("https://flatironschool.com/", headers=headers)
+
+doc = BeautifulSoup(html.text, 'html.parser')
+# print(doc.select('.cli-switch'))
+# print(doc.select('.cli-switch'))
+
+courses = doc.select('div')
+
+for course in courses: 
+  	print(course.contents[0].strip())
